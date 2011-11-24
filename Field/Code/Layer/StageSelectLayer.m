7ac97efc5cc6a9d33d519beff95d3a15615ea483
Asata MacMini <asata@rcttd.com>
2011-11-24 22:42:58 +0900

    스테이지 선택 화면 갱신
    - 스테이지 수 증가 (스테이지 내용은 아직 적용 안함)
    - 테스트를 위해 전체 스테이지 개방됨
    - 선 긋기 완료

--- Field/Code/Layer/StageSelectLayer.m
+++ Field/Code/Layer/StageSelectLayer.m
@@ -17,30 +17,46 @@
 }
 
 - (void) onEnterTransitionDidFinish {
+    File *file = [[File alloc] init];
+    NSString *path = [file loadFilePath:FILE_STAGE_PLIST];
+    [file loadGameData:path];
+    
+    NSMutableDictionary *stageData  = [[[commonValue sharedSingleton] getGameData] objectForKey:@"Info"];
+    NSInteger           stageNumber = [[stageData objectForKey:@"StageNum"] intValue];
+    
     CCSprite *background = [CCSprite spriteWithFile:FILE_STAGE_IMG];
-    [self addChild:background];
+    [self addChild:background z:-1];
     
     pZoom = [PinchZoomLayer initPinchZoom:background];
     [pZoom scaleToInit:1];
     
-    CCSprite *stage1 = [CCSprite spriteWithFile:@"fire.png"];
-    [stage1 setPosition:ccp(200, 200)];
-    [stage1 setTag:1];
-    [menuList addObject:stage1];
-    [pZoom addChild:stage1];
+    ccColor4B myColor = ccc4(76, 123, 23, 150);
+    CCRibbon *ribbon = [CCRibbon ribbonWithWidth:10 image:@"green.png" length:10.0 color:myColor fade:1.0f];
+    [pZoom addChild:ribbon z:8];
     
-    CCSprite *stage2 = [CCSprite spriteWithFile:@"fire.png"];
-    [stage2 setPosition:ccp(400, 200)];
-    [stage2 setTag:2];
-    [menuList addObject:stage2];
-    [pZoom addChild:stage2];
-    
-    CCSprite *stage3 = [CCSprite spriteWithFile:@"fire.png"];
-    [stage3 setPosition:ccp(600, 200)];
-    [stage3 setTag:3];
-    [menuList addObject:stage3];
-    [pZoom addChild:stage3];
-    
+    for (NSInteger i = 1; i <= stageNumber; i++) {
+        NSString *stageNum = [NSString stringWithFormat:@"%d", i];
+        NSMutableDictionary *stageData = [[[commonValue sharedSingleton] getGameData] objectForKey:stageNum];
+        BOOL isClear = [[stageData objectForKey:@"isCleared"] boolValue];
+        NSInteger positionX = [[stageData objectForKey:@"PositionX"] intValue];
+        NSInteger positionY = [[stageData objectForKey:@"PositionY"] intValue];
+        
+        CCSprite *stage;
+        
+        if (isClear) {
+            stage = [CCSprite spriteWithFile:@"btn_stage_select_1.png"];
+        } else {
+            stage = [CCSprite spriteWithFile:@"btn_stage_select_2.png"];
+        }
+        
+        [ribbon addPointAt:ccp(positionX, positionY - 20) width:3];
+        
+        [stage setPosition:ccp(positionX, positionY)];
+        [stage setTag:i];
+        [menuList addObject:stage];
+        [pZoom addChild:stage z:10];
+        [stage release];
+    }
 }
 
 - (void) ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -52,12 +68,14 @@
     CGPoint location = [pZoom convertTouchToNodeSpace:touch];
     
     for (CCSprite *tSprite in menuList) {
-        if (tSprite.position.x - 16 <= location.x && tSprite.position.x + 16 >= location.x &&
-            tSprite.position.y - 16 <= location.y && tSprite.position.y + 16 >= location.y) {
+        if (tSprite.position.x - 36 <= location.x && tSprite.position.x + 36 >= location.x &&
+            tSprite.position.y - 36 <= location.y && tSprite.position.y + 36 >= location.y) {
             [[commonValue sharedSingleton] setStageLevel:[tSprite tag]];
             [(CCLayerMultiplex*)parent_ switchTo:GAME_LAYER];
         }
     }
+    
+    NSLog(@"%f %f", location.x, location.y);
 }
 
 - (void) stageStart:(id)sender {
